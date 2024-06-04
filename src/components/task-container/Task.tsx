0b7298b184e90d808d3923f48f360d8659d638a0
samkhanhduy <samkhanhduy2002@gmail.com>
2024-06-04 16:31:47 +0700

    add delete confirm modal

--- src/components/task-container/Task.tsx
+++ src/components/task-container/Task.tsx
@@ -4,7 +4,8 @@ import ImportantTaskIcon from "../../assets/icons/important-star.svg";
 import UnimportantTaskIcon from "../../assets/icons/unimportant-task.svg";
 import DeleteIcon from "../../assets/icons/trash-bin.svg";
 import EditIcon from "../../assets/icons/edit-task.svg";
-
+import { useState } from "react";
+import { Modal } from 'antd';
 export default function Task(
     {
         title, 
@@ -29,45 +30,70 @@ export default function Task(
         changeProgress: () => void
     }
 ) {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+      setIsModalOpen(true);
+    };
+    const handleOk = () => {
+      setIsModalOpen(false);
+      deleteTask();
+    };
+    const handleCancel = () => {
+      setIsModalOpen(false);
+    };
+
+
     return (
-        <div className={display !== "row" ?  "task isCard" : "task isRow"}>
-            
-            <div className="task-title">{title}</div>
-            
-            <div className="task-desciption">{description}</div>
-            
-            <div className="task-date">
-                <img src={CalendarIcon} alt="" className="icon" />
-                {date}
-            </div>
+        <div>
+            <div className={display !== "row" ?  "task isCard" : "task isRow"}>
+                
+                <div className="task-title">{title}</div>
+                
+                <div className="task-desciption">{description}</div>
+                
+                <div className="task-date">
+                    <img src={CalendarIcon} alt="" className="icon" />
+                    {date}
+                </div>
 
-            <div className="task-others-info">
-                {isComplete ? 
-                    <div className="complete-tag cursor-pointer" onClick={changeProgress}>
-                        completed
-                    </div> :
-                    <div className="uncomplete-tag cursor-pointer" onClick={changeProgress}>
-                        uncompleted
+                <div className="task-others-info">
+                    {isComplete ? 
+                        <div className="complete-tag cursor-pointer" onClick={changeProgress}>
+                            completed
+                        </div> :
+                        <div className="uncomplete-tag cursor-pointer" onClick={changeProgress}>
+                            uncompleted
+                        </div>
+                    }
+
+                    <div className="other-action flex flex-nowrap">
+                        <img src={isImportant ? ImportantTaskIcon : UnimportantTaskIcon} 
+                            alt=""
+                            className="icon" 
+                            onClick={changeImportant}
+                        />
+
+                        <img src={DeleteIcon} alt="" 
+                            className="delete-task icon"
+                            onClick={showModal}
+                        />
+
+                        <img src={EditIcon} alt="" 
+                            className="icon"
+                        />
                     </div>
-                }
-
-                <div className="other-action flex flex-nowrap">
-                    <img src={isImportant ? ImportantTaskIcon : UnimportantTaskIcon} 
-                        alt=""
-                        className="icon" 
-                        onClick={changeImportant}
-                    />
-
-                    <img src={DeleteIcon} alt="" 
-                        className="delete-task icon"
-                        onClick={deleteTask}
-                    />
-
-                    <img src={EditIcon} alt="" 
-                        className="icon"
-                    />
                 </div>
             </div>
+                
+            <Modal 
+                title="Delete Task" 
+                open={isModalOpen} 
+                style={{ top: '50%' }}
+                onOk={handleOk} 
+                onCancel={handleCancel}></Modal>
+          
+
         </div>
     );
 }
